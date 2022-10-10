4acd8c99ff15692b815405a1d965433c219ebef2
Vitor Nicacio <vitornicacio10@hotmail.com>
2022-10-10 00:46:40 -0300

    realizar troca de nome de variavel

--- MoneyExtension/Money.cs
+++ MoneyExtension/Money.cs
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 namespace MoneyExtension;
 public static class Money
 {
-    public static int ToCents(this decimal amount)
+    public static int ToCents(this decimal value)
     {
-        if( amount < 0)
+        if( value < 0)
             return 0;
 
-        var text = amount
+        var valueInText = value
         .ToString("N2")
         .Replace(",", "")
         .Replace(".", "");
 
-        if(string.IsNullOrEmpty(text))
+        if(string.IsNullOrEmpty(valueInText))
         {
             return 0;
         }
 
-        int.TryParse(text, out var result);
+        int.TryParse(valueInText, out var result);
         return result;
     }
 }
